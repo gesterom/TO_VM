e642e0f9cd0f089d6a48e8cdb4710a4a3d6066e6
Mateusz Grajcar <rasarex@o2.pl>
2019-12-15 01:31:16 +0100

    removing unnesessery files

--- io/include/printf.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define uint  unsigned int
-
-int mprintf(char* format, ...);
-uint stlen(char* str);
-int itos(int val,char* buffer, int base);
-char* reverse(char* str, uint  len);
